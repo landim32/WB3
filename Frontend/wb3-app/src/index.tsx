44b670382afb2f89addd670c2741181ed073b2bc
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-11-25 02:40:57 -0300

    first deploy

--- Frontend/wb3-app/src/index.tsx
+++ Frontend/wb3-app/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import env from 'react-dotenv';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={env.SITE_BASENAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
