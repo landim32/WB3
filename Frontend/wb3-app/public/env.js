465fc554ad3486dc823413827779c6ba7f823ead
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-11-29 16:46:34 -0300

    deploy on /wb3

--- Frontend/wb3-app/public/env.js
+++ Frontend/wb3-app/public/env.js
@@ -2,7 +2,7 @@ window.env = {
   "NODE_PATH": "./src",
   "REACT_APP_PRODUCTION": "0",
   "PUBLIC_URL": "/wb3",
-  "PORT": "80",
+  "PORT": "443",
   "SITE_BASENAME": "/wb3",
   "REACT_APP_NETWORK": "97",
   "REACT_APP_CHAIN_URL": "https://data-seed-prebsc-1-s1.binance.org:8545/",
